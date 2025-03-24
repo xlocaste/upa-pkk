bdbf95386d7dfd30e13ce38f88e5bb4aeb090cdf
xlocaste <irfandi.di122@gmail.com>
2025-03-24 16:58:56 +0700

    feat:store controller mahasiswa

--- app/Http/Controllers/Alumni/MahasiswaController.php
+++ app/Http/Controllers/Alumni/MahasiswaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Alumni;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Mahasiswa;
+use App\Http\Requests\Product\StoreRequest;
 
 class MahasiswaController extends Controller
 {
@@ -15,5 +16,17 @@ class MahasiswaController extends Controller
         return inertia::render('Alumni/Mahasiswa/List', [
             'Mahasiswa' => $daftarMahasiswa
         ]);
+    }
+
+    public function store(StoreRequest $request)
+    {
+        $mahasiswa = Mahasiswa::create([
+            'nama'=>$nama,
+            'nim'=>$nim,
+            'semester'=>$semester,
+            'ipk'=>$ipk,
+        ]);
+
+        return redict()->route('mahasiswa.index');
     }
 }
