ee236952e2fedf7b805c45912f9683713e0772c3
xlocaste <irfandi.di122@gmail.com>
2025-04-06 14:29:01 +0700

    fix:login change logout when user login on navbar components

--- resources/js/Components/Navbar.tsx
+++ resources/js/Components/Navbar.tsx
@@ -1,12 +1,20 @@
 import ApplicationLogo from '@/Components/ApplicationLogo';
-import { Link } from "@inertiajs/react";
+import { Link, usePage } from "@inertiajs/react";
 import { ReactNode } from "react";
 
 interface NavbarProps {
     children: ReactNode;
 }
 
+interface User {
+    id: number;
+    name: string;
+    email: string;
+}
+
 export default function Navbar({ children }: NavbarProps) {
+    const { auth } = usePage().props as unknown as { auth: { user: User | null } };
+
     return (
         <>
             <nav className="bg-gray-100">
@@ -19,40 +27,52 @@ export default function Navbar({ children }: NavbarProps) {
                             <div className="flex space-x-4">
                                 <Link
                                     href={route('mahasiswa.index')}
-                                    className="px-4 py-2 text-sm text-gray-700 hover:underline focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500"
+                                    className="px-4 py-2 text-sm text-gray-700 hover:underline"
                                 >
                                     Mahasiswa
                                 </Link>
                                 <Link
                                     href={route('mahasiswa.index')}
-                                    className="px-4 py-2 text-sm text-gray-700 hover:underline focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500"
+                                    className="px-4 py-2 text-sm text-gray-700 hover:underline"
                                 >
                                     Alumni
                                 </Link>
                                 <Link
                                     href={route('login')}
-                                    className="px-4 py-2 text-sm text-gray-700 hover:underline focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500"
+                                    className="px-4 py-2 text-sm text-gray-700 hover:underline"
                                 >
                                     Lowongan Kerja
                                 </Link>
                                 <Link
                                     href={route('login')}
-                                    className="px-4 py-2 text-sm text-gray-700 hover:underline focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500"
+                                    className="px-4 py-2 text-sm text-gray-700 hover:underline"
                                 >
                                     Inkubasi
                                 </Link>
                                 <Link
                                     href={route('login')}
-                                    className="px-4 py-2 text-sm text-gray-700 hover:underline focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500"
+                                    className="px-4 py-2 text-sm text-gray-700 hover:underline"
                                 >
                                     Pra Inkubasi
                                 </Link>
-                                <Link
-                                    href={route('login')}
-                                    className="px-4 py-2 text-sm text-gray-700 hover:underline focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500"
-                                >
-                                    Log in
-                                </Link>
+
+                                {auth.user ? (
+                                    <Link
+                                        method="post"
+                                        href={route('logout')}
+                                        as="button"
+                                        className="px-4 py-2 text-sm text-gray-700 hover:underline"
+                                    >
+                                        Log out
+                                    </Link>
+                                ) : (
+                                    <Link
+                                        href={route('login')}
+                                        className="px-4 py-2 text-sm text-gray-700 hover:underline"
+                                    >
+                                        Log in
+                                    </Link>
+                                )}
                             </div>
                         </div>
                     </div>
